897a4a960f27f242ddd2204f5be35cb8ea424cbb
huuvui <113670992+huuvui@users.noreply.github.com>
2023-04-06 10:28:15 +0700

    Cập nhật lại src

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,15 +1,13 @@
 import axios from "axios";
-const commonConfig = {
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
+const  commonConfig = {
+    headers: { 
+        "Content-Type": "application/json", 
+        Accept: "application/json", 
     },
 };
-export default (baseURL) => {
-    return axios.create({
-        baseURL,
-        ...commonConfig,
-    });
+export default (baseURL) => { 
+    return axios.create({ 
+        baseURL, 
+        ...commonConfig, 
+    }); 
 };
-
-
